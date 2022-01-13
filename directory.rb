935391d6aa1d5ecc3728fc8cdff62643264b3619
Oliver Cutting <oliver.cutting@gmail.com>
2022-01-13 14:16:16 +0000

    Added initial search functionality

--- directory.rb
+++ directory.rb
@@ -4,14 +4,14 @@ def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   # get the first name
-  name = gets.chomp
+  name = gets.chomp.capitalize
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
     # get another name from the user
-    name = STDIN.gets.chomp
+    name = STDIN.gets.chomp.capitalize
   end
   @students
 end
@@ -26,8 +26,10 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "4. Load the list from students.csv"
+  puts "3. Search students by initial"
+  puts "4. Show students whose names are shorter than X characters"
+  puts "5. Save the list to students.csv"
+  puts "6. Load the list from students.csv"
   puts "9. Exit" # 9 because we'll be adding more items
 end
 
@@ -37,6 +39,30 @@ def show_students
   print_footer
 end
 
+def print_specific_initial
+  puts "Which initial would you like to search through?"
+  initial = gets.chomp
+  @students.each do |student| 
+    student.each do |key, value|
+      if key == :name && value.chr == initial 
+        puts value 
+      end
+    end
+  end
+end
+
+def print_shorter_than
+  puts "How many letters would you like to see?"
+  input = gets.chomp.to_i
+  @students.each do |student|
+    student.each do |key, value|
+      if key == :name && value.length <= input
+        puts value
+      end
+    end
+  end
+end
+
 def process(selection)
   case selection
   when "1"
@@ -44,8 +70,12 @@ def process(selection)
   when "2"
     show_students
   when "3"
-    save_students
+    print_specific_initial
   when "4"
+    print_shorter_than
+  when "5"
+    save_students
+  when "6"
     load_students
   when "9"
     exit # this will cause the program to terminate
@@ -60,8 +90,10 @@ def print_header
 end
 
 def print_student_list
-  @students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  i = 0
+  while i < @students.count
+    puts "#{i + 1}. #{@students[i][:name]} (#{@students[i][:cohort]} cohort)"
+    i += 1
   end
 end
 
